d1d641872158fb89ef8e70ef5e201451ae2711e5
Jon Schulberger <jonathan.schulberger@mymail.champlain.edu>
2018-04-03 01:37:29 -0400

    Formatting & documentation

--- display/startup.sh
+++ display/startup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# Wait until frontend is ready
+echo "Waiting for frontend..."
 echo exit | telnet frontend 4200 &> /dev/null
 while [ $? -ne 0 ]; do
   echo exit | telnet frontend 4200 &> /dev/null
 done
+echo "Frontend is alive!"
 
+# Start main script
 /wpe-init
